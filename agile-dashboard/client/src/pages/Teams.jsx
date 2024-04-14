25a3e6463de6640a92cf6ce4c06041b63c12cdc1
JoeDunne212 <dunnejoe65@gmail.com>
2024-04-14 16:25:47 +0100

    Clickable cards by wrapping in button

--- agile-dashboard/client/src/pages/Teams.jsx
+++ agile-dashboard/client/src/pages/Teams.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/NavBar/navbar';
 import Footer from '../components/footer.tsx';
-import TeamCardRow from '../components/Layout/TeamCardRow.jsx';
 import TeamCard from '../components/Layout/TeamCard.jsx';
 import imgPath from '../img/cat.jpg'
 const Teams = (props) => {
+  const navigate = useNavigate();
+  
+  const navigationHandler = (pathTeamName) => {
+    //navigate(`teams/${pathTeamName}`)
+    console.log("hit here hello joseph");
+  }
+  
   return (
     <div className="bg-neutral-800 text-white flex flex-col h-screen">
       <Navbar></Navbar>
-      <body className='flex-1 overflow-auto w-full bg-neutral-600 justify-center align-bottom'>
-     
-     
+      <body className='flex-1 overflow-auto w-full bg-neutral-600 justify-center align-bottom'> 
       <div className='bg- bg-max-w-4xl mt-24 w-full h-80 text-center'>
         
             <div className='flex justify-center bg-white'>
-                <TeamCard name='Team A' url={imgPath}></TeamCard>
-                <TeamCard name='Team B' url={imgPath}></TeamCard>
-                <TeamCard name='Team C' url={imgPath}></TeamCard>
+                <TeamCard name='Team A' url={imgPath} navHandler={navigationHandler}></TeamCard>
+                <TeamCard name='Team B' url={imgPath} navHandler={navigationHandler}></TeamCard>
+                <TeamCard name='Team C' url={imgPath} navHandler={navigationHandler}></TeamCard>
             </div>
             <div className='flex justify-center bg-white'>
-              <TeamCard name='Team D' url={imgPath}></TeamCard>
-              <TeamCard name='Team E' url={imgPath}></TeamCard>
-              <TeamCard name='Team F' url={imgPath}></TeamCard>
+              <TeamCard name='Team D' url={imgPath} navHandler={navigationHandler} ></TeamCard>
+              <TeamCard name='Team E' url={imgPath} navHandler={navigationHandler} ></TeamCard>
+              <TeamCard name='Team F' url={imgPath} navHandler={navigationHandler} ></TeamCard>
             </div>
         </div>
         
